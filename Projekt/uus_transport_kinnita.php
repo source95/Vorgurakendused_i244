da64cbc508bc2b4fc777def9ea842adc37506b2d
source95 <filip.fjodorov@tptlive.ee>
2017-04-10 23:27:31 +0300

    Add files via upload
    connection to mysql
    form2 for confirmation and form3 for save data in db

---
+++ Projekt/uus_transport_kinnita.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+	 
+if (isset($_POST['auto_regnr'])){
+ if (empty($_POST['auto_regnr'])
+ || empty($_POST['juhi_nimi'])
+ || empty($_POST['dok_nr'])){ 
+ // Setting error message
+ $_SESSION['error'] = "Mandatory field(s) are missing, Please fill it again";
+ header("location: page1_form.php"); // Redirecting to first page 
+ } else {
+ foreach ($_POST as $key => $value) {
+ $_SESSION['post'][$key] = htmlspecialchars($value);
+ }
+ }  
+} else {
+ if (empty($_SESSION['error_page2'])) {
+ header("location: page1_form.php");//redirecting to first page
+ }
+}		 
+?>
+<!DOCTYPE HTML>
+<html>
+ <head>
+ <title>Uue transpordi registreerimise kinnitus</title>
+ <link rel="stylesheet" href="style.css" />
+ </head>
+ <body>
+ <div class="container">
+ <div class="main">
+ <h2>Uue transpordi registreerimise kinnitus</h2><hr/>
+ <span id="error">
+<?php
+// To show error of page 2.
+if (!empty($_SESSION['error_page2'])) {
+ echo $_SESSION['error_page2'];
+ unset($_SESSION['error_page2']);
+}
+?>
+ </span>
+
+<?php 
+$dt = new DateTime();
+$nameErr ="";
+
+/*extract($_SESSION['post']); // Function to extract array.
+unset($_SESSION['post']); // Destroying session.
+*/
+/*if (isset($_POST['auto_regnr']) && $_POST['auto_regnr']!="") {
+    $auto_reg_nr=htmlspecialchars($_POST['auto_regnr']);
+  }
+if (isset($_POST['juhi_nimi']) && $_POST['juhi_nimi']!="") {
+    $autojuhi_nimi=htmlspecialchars($_POST['juhi_nimi']);
+  }
+if (isset($_POST['dok_nr']) && $_POST['dok_nr']!="") {
+    $dokument_nr=htmlspecialchars($_POST['dok_nr']);
+  } 
+if (isset($_POST['kaup_sisse']) && $_POST['kaup_sisse']!="") {
+    $kaup_sisse=htmlspecialchars($_POST['kaup_sisse']);
+  } 
+if (isset($_POST['kaup_valja']) && $_POST['kaup_valja']!="") {
+    $kaup_valja=htmlspecialchars($_POST['kaup_valja']);
+  } */
+
+echo $auto_regnr;
+echo "<br>";
+echo $juhi_nimi; 
+echo "<br>";
+echo $dok_nr; 
+echo "<br>";
+echo $kaup_sisse; 
+echo "<br>";
+echo $kaup_valja; 
+echo "<br>";
+
+echo $dt->format('Y-m-d H:i:s');
+?>
+<form action="uus_transport_salvesta.php" method="post">
+<input type="submit" value="Kinnita" />
+</form>
+ 
+ </div>
+ </div>
+ </body>
+</html>
---
+++ Projekt/uus_transport_salvesta.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+ <title>Salvesta</title>
+ <link rel="stylesheet" href="style.css" />
+ </head>
+ <body>
+ <div class="container">
+ <div class="main">
+ <h2>Salvest1</h2>
+ <?php
+ session_start();
+extract($_SESSION['post']); // Function to extract array.
+unset($_SESSION['post']); // Destroying session.
+
+ include 'db_connect.php';
+ 
+ // An insertion query. $result will be `true` if successful
+  $result = db_query("INSERT INTO uus_transport (auto_regnr, juhi_nimi, dok_nr) VALUES ('$auto_regnr','$juhi_nimi', '$dok_nr')");
+   if($result === false) {
+   		$error = db_error();
+   		echo '<p><span>Form Submission Failed..!!</span></p>';
+        // Handle failure - log the error, notify administrator, etc.
+    } else {
+    	echo '<p><span id="success">Form Submitted successfully..!!</span></p>';
+        // We successfully inserted a row into the database
+    }
+
+
+
+ ?>
+ </div>
+ </div>
+ </body>
+</html>
