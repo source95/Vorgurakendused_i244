33e0466859087f757f4a0bb8044b3c2d6bd0d518
source95 <filip.fjodorov@tptlive.ee>
2017-05-22 23:37:02 +0300

    lisatud vormi saatmise käigus kellaaja fikseerimine datetime

--- Projekt/uus_transport_salvesta.php
+++ Projekt/uus_transport_salvesta.php
@@ -2,7 +2,7 @@
 <html>
  <head>
  <title>Salvesta</title>
- <link rel="stylesheet" href="style.css" />
+ <link rel="stylesheet" href="uus_transport.css" />
  </head>
  <body>
  <div class="container">
@@ -14,9 +14,11 @@ extract($_SESSION['post']); // Function to extract array.
 unset($_SESSION['post']); // Destroying session.
 
  include 'db_connect.php';
+
+ $mysqltime = date ("Y-m-d H:i:s"); 
  
  // An insertion query. $result will be `true` if successful
-  $result = db_query("INSERT INTO uus_transport (auto_regnr, juhi_nimi, dok_nr) VALUES ('$auto_regnr','$juhi_nimi', '$dok_nr')");
+  $result = db_query("INSERT INTO uus_transport (auto_regnr, juhi_nimi, dok_nr, dateposted) VALUES ('$auto_regnr','$juhi_nimi', '$dok_nr', '$mysqltime')");
    if($result === false) {
    		$error = db_error();
    		echo '<p><span>Form Submission Failed..!!</span></p>';
