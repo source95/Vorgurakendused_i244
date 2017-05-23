3480a3d4e3084c590b3e41c51c80bb31e5d77fbd
source95 <filip.fjodorov@tptlive.ee>
2017-05-24 01:12:26 +0300

    Update kontroller.php
    Lisatud veel mõned case'id

--- Projekt/kontroller.php
+++ Projekt/kontroller.php
@@ -1,32 +1,46 @@
 <?php 
-require_once('head.html');
+require_once('functions.php');
+session_start();
+connect_db();
+require_once('views/head.html');
  ?>
 
 <?php 
 
 
 if (!empty($_GET)){
-$leht = $_GET["page"];
+$leht = htmlspecialchars($_GET["page"]);
 
 switch ($leht) {
 	case 'uus_transport':
-		require_once('uus_transport.php');
+			require_once('uus_transport.php');
 		break;
 	case 'home':
-		require_once('home.html');
+			require_once('views/home.html');
 		break;
 	case 'query':
-		require_once('query.php');
+			require_once('query.php');
 			break;	
 	case 'tulemus':
-		require_once('tulemus.php');
+			require_once('tulemus.php');
 		break;
+	case 'login':
+			logi();
+		break;
+	case "logout":
+			logout();
+	break;
+	case 'loggedin':
+			require_once('views/loggedin.html');
+		break;	
+	case 'kontakt':
+			require_once('views/kontakt.html');
+			break;	
 	default:
 		# code...
 		break;
 	}
 }
 
+require_once('views/foot.html'); 
 ?>
-
-<?php require_once('foot.html'); ?>
