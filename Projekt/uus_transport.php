3a775c006605b6c196ad0c66732103536c39864b
source95 <filip.fjodorov@tptlive.ee>
2017-05-28 22:08:23 +0300

    Lisatud lahtrid ja korrastatud kood

--- Projekt/uus_transport.php
+++ Projekt/uus_transport.php
@@ -1,11 +1,12 @@
-<!-- <?php
-//session_start(); // Session starts here. Exist in kontroller.php
-?> -->
-<link rel="stylesheet" href="css/uus_transport.css" />
+<?php
+if (empty($_SESSION['user'])) {
+        header("Location: ?page=login");
+    } else {  ?>
 
+<link rel="stylesheet" href="css/uus_transport.css" />
  <div class="container">
  <div class="main">
- <h2>Uue transpordi registreerimine</h2>
+ <h2>Transpordi registreerimine</h2>
  <span id="error">
 <!--  Initializing Session for errors -->
  <?php
@@ -16,18 +17,22 @@
  ?>
  </span>
 <!-- <h1>Uue transpordi registreerimine</h1> -->
-<form name="uus_transport" method="post" action="uus_transport_kinnita.php" >
+<form name="uus_transport" method="post" action="?page=uus_transport_kinnita" >
     Sõiduki reg NR: <input type="text" name="auto_regnr" required><br>
     Autojuhi nimi  <input type="text" name="juhi_nimi" required><br>
     Dokumendi NR  <input type="text" name="dok_nr" required><br>
+    Kauba vedaja  <input type="text" name="vedaja" required><br>
+    Estakaad  <input type="number" name="estakaad" min="1" max="25" required><br>
     <!-- Kaup <input type="radio" name="kaup" value="sisse" > Sisse
      <input type="radio" name="kaup" value="välja"> Välja<br> -->
-     Kaup <input type="checkbox" name="kaup_sisse" value="sisse"> Sisse 
-          <input type="checkbox" name="kaup_valja" value="välja"> Välja <br>
+     Kaup <br>
+     <input type="checkbox" name="kaup_sisse" value="jah"> Sisse 
+     <input type="checkbox" name="kaup_valja" value="jah"> Välja <br>
     <br>
-     <input type="reset" value="Reset" />
+     <input type="reset" value="Kustuta" />
     <input type="submit" value="Edasi" name="submit" />
   </form>
 <br>
  </div>
  </div>
+<?php } ?>
